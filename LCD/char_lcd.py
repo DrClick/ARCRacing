ed5584620ae2651a2767695a13e8e6f8cb518b94
Thomas <drclick@mac.com>
2017-07-14 06:25:05 +0000

    updated char_lcd

--- LCD/char_lcd.py
+++ LCD/char_lcd.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python
-# Example using a character LCD connected to a Raspberry Pi or BeagleBone Black.
 import time
-
 import Adafruit_CharLCD as LCD
 
 
@@ -14,20 +12,7 @@ lcd_d6        = 5
 lcd_d7        = 11
 lcd_backlight = 4
 
-# BeagleBone Black configuration:
-# lcd_rs        = 'P8_8'
-# lcd_en        = 'P8_10'
-# lcd_d4        = 'P8_18'
-# lcd_d5        = 'P8_16'
-# lcd_d6        = 'P8_14'
-# lcd_d7        = 'P8_12'
-# lcd_backlight = 'P8_7'
-
-# Define LCD column and row size for 16x2 LCD.
-#lcd_columns = 16
-#lcd_rows    = 2
-
-# Alternatively specify a 20x4 LCD.
+# device config
 lcd_columns = 20
 lcd_rows    = 4
 
@@ -35,51 +20,14 @@ lcd_rows    = 4
 lcd = LCD.Adafruit_CharLCD(lcd_rs, lcd_en, lcd_d4, lcd_d5, lcd_d6, lcd_d7,
                            lcd_columns, lcd_rows, lcd_backlight)
 
-lcd.enable_display(True)
+
 # Print a two line message
-lcd.message('Hello\nworld!')
+lcd.message('Vector79\nAutonomous RC Racing\nIP: address \nCPU: Temp')
+
+#lcd.message('\n     I love you\n      gorgeous\n')
 
 # Wait 5 seconds
-time.sleep(5.0)
+#time.sleep(5.0)
 
-# Demo showing the cursor.
-lcd.clear()
-lcd.show_cursor(True)
-lcd.message('Show cursor')
+#lcd.clear()
 
-time.sleep(5.0)
-
-# Demo showing the blinking cursor.
-lcd.clear()
-lcd.blink(True)
-lcd.message('Blink cursor')
-
-time.sleep(5.0)
-
-# Stop blinking and showing cursor.
-lcd.show_cursor(False)
-lcd.blink(False)
-
-# Demo scrolling message right/left.
-lcd.clear()
-message = 'Scroll'
-lcd.message(message)
-for i in range(lcd_columns-len(message)):
-    time.sleep(0.5)
-    lcd.move_right()
-for i in range(lcd_columns-len(message)):
-    time.sleep(0.5)
-    lcd.move_left()
-
-# Demo turning backlight off and on.
-lcd.clear()
-lcd.message('Flash backlight\nin 5 seconds...')
-time.sleep(5.0)
-# Turn backlight off.
-lcd.set_backlight(0)
-time.sleep(2.0)
-# Change message.
-lcd.clear()
-lcd.message('Goodbye!')
-# Turn backlight on.
-lcd.set_backlight(1)
